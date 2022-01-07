04ecc3e961cdd49da33640b7ffb5a46b74d2d030
Seb W <thelayeredmind@gmail.com>
2022-01-07 11:02:00 +0000

    JS Environment for Data Vis.

---
+++ visualization/index.js
@@ -0,0 +1,7 @@
+import * as d3 from 'd3'
+import * as sc from 'scrollama'
+
+
+console.log("hello")
+console.log(d3)
+console.log(sc)
---
+++ visualization/webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: './index.js',
+  output: {
+    filename: 'app.js',
+    path: __dirname
+  },
+};
