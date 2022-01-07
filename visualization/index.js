9ec2aee6a48311db6c6c5ba97a180df8e0cfd863
Seb W <thelayeredmind@gmail.com>
2022-01-07 14:38:14 +0000

    Divide datasets by timeframes

--- visualization/index.js
+++ visualization/index.js
@@ -2,4 +2,11 @@ import * as d3 from 'd3'
 import * as sc from 'scrollama'
 
 const scroller = sc();
-console.log("test")
+
+
+// X Axis -> Runtime
+// Y Axis -> Rating (Normalized: User, Critics)
+// Semi-Transparent Serpent 1 (Users) Thickness -> Vote Amount on IMDB
+// Sharp Line 2 (Critics)
+// Very light (golden) Background Serpent -> Gross Income
+// Single Year Time Span on Top of Graph (4 Steps = Data Sets) -> Animation over Time
